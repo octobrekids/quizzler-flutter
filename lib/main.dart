5fd26374a2df209b6cbae88858094be7de81359b
octobrekids <chnrd.k@gmail.com>
2021-06-06 22:55:16 +0700

    add quizbrain class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() {
   runApp(Quizzler());
@@ -32,14 +34,6 @@ class _QuizPageState extends State<QuizPage> {
 
   int questionNumber = 0;
 
-  List<Question> questionBank = [
-    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-    Question(
-        q: 'Approximately one quarter of human bones are in the feet.',
-        a: true),
-    Question(q: 'A slug\'s blood is green.', a: true),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -52,7 +46,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -76,7 +70,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   bool correctAnswer =
-                      questionBank[questionNumber].questionAnswer;
+                      quizBrain.questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == true) {
                     print('right');
                     scoreKeeper.add(
@@ -101,7 +95,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 setState(() {
                   bool correctAnswer =
-                      questionBank[questionNumber].questionAnswer;
+                      quizBrain.questionBank[questionNumber].questionAnswer;
                   if (correctAnswer == false) {
                     scoreKeeper.add(
                       Icon(Icons.check, color: Colors.green),
